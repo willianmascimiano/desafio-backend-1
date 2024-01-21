a90bcf843eeac26cab5a66ce5f5f3006728e874d
Wlilian Mascimiano <willian@wsa.com.br>
2024-01-21 06:45:35 -0300

    Camada auxiliar de Notificação.

---
+++ src/main/java/com/desafio/primeiro/strategy/NotificarStrategy.java
@@ -0,0 +1,17 @@
+package com.desafio.primeiro.strategy;
+
+public class NotificarStrategy {
+
+    public void enviarSms(){
+        System.out.println("Enviar SMS...");
+    }
+
+    public void enviarEmail(){
+
+        System.out.println("Enviar e-mail");
+    }
+
+
+
+
+}
