b8034afca128a816806ca065ec0efce74cabe47d
Wlilian Mascimiano <willian@wsa.com.br>
2024-01-21 06:46:53 -0300

    Repositório java.

--- src/main/java/com/desafio/primeiro/repository/UsuarioRepository.java
+++ src/main/java/com/desafio/primeiro/repository/UsuarioRepository.java
@@ -3,8 +3,13 @@ package com.desafio.primeiro.repository;
 import com.desafio.primeiro.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import javax.swing.text.html.Option;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, UUID> {
+
+    Optional<Usuario> findUsuarioByNome(String nome);
+    Optional<Usuario> findUsuarioById(UUID id);
 
 }
